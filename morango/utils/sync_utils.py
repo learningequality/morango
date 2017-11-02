b12b9687e9d96779f5a53da885be89659bfd587d
Rafael Aguayo <ralphaguayo22@gmail.com>
2017-11-02 11:28:42 -0700

    Update last_saved_bys and rmcs for deleted models.

--- morango/utils/sync_utils.py
+++ morango/utils/sync_utils.py
@@ -33,8 +33,6 @@ def _serialize_into_store(profile, filter=None):
     current_id = InstanceIDModel.get_current_instance_and_increment_counter()
 
     with transaction.atomic():
-        defaults = {'instance_id': current_id.id, 'counter': current_id.counter}
-
         # create Q objects for filtering by prefixes
         prefix_condition = None
         if filter:
@@ -55,6 +53,7 @@ def _serialize_into_store(profile, filter=None):
                     # if store record dirty and app record dirty, append store serialized to conflicting data
                     if store_model.dirty_bit:
                         store_model.conflicting_serialized_data = store_model.serialized + "\n" + store_model.conflicting_serialized_data
+                        store_model.dirty_bit = False
 
                     # set new serialized data on this store model
                     ser_dict = json.loads(store_model.serialized)
@@ -62,8 +61,9 @@ def _serialize_into_store(profile, filter=None):
                     store_model.serialized = DjangoJSONEncoder().encode(ser_dict)
 
                     # create or update instance and counter on the record max counter for this store model
-                    defaults.update({'store_model_id': store_model.id})
-                    RecordMaxCounter.objects.update_or_create(defaults=defaults, instance_id=current_id.id, store_model_id=store_model.id)
+                    RecordMaxCounter.objects.update_or_create(defaults={'counter': current_id.counter},
+                                                              instance_id=current_id.id,
+                                                              store_model_id=store_model.id)
 
                     # update last saved bys for this store model
                     store_model.last_saved_instance = current_id.id
@@ -90,8 +90,7 @@ def _serialize_into_store(profile, filter=None):
                         kwargs.update({'_self_ref_fk': self_ref_fk_value or ''})
                     # create store model and record max counter for the app model
                     new_store_records.append(Store(**kwargs))
-                    defaults.update({'store_model_id': app_model.id})
-                    new_rmc_records.append(RecordMaxCounter(**defaults))
+                    new_rmc_records.append(RecordMaxCounter(store_model_id=app_model.id, instance_id=current_id.id, counter=current_id.counter))
 
             # bulk create store and rmc records for this class
             Store.objects.bulk_create(new_store_records)
@@ -100,10 +99,20 @@ def _serialize_into_store(profile, filter=None):
             # set dirty bit to false for all instances of this model
             klass_queryset.update(update_dirty_bit_to=False)
 
-        # update deleted flags based on DeletedModels
+        # get list of ids of deleted models
         deleted_ids = DeletedModels.objects.filter(profile=profile).values_list('id', flat=True)
-        Store.objects.filter(id__in=deleted_ids).update(deleted=True)
+        # update last_saved_bys and deleted flag of all deleted store model instances
+        deleted_store_records = Store.objects.filter(id__in=deleted_ids)
+        deleted_store_records.update(dirty_bit=False, deleted=True, last_saved_instance=current_id.id, last_saved_counter=current_id.counter)
+        # update rmcs counters for deleted models that have our instance id
+        RecordMaxCounter.objects.filter(instance_id=current_id.id, store_model_id__in=deleted_ids).update(counter=current_id.counter)
+        # get a list of deleted model ids that don't have an rmc for our instance id
+        new_rmc_ids = deleted_store_records.exclude(recordmaxcounter__instance_id=current_id.id).values_list("id", flat=True)
+        # bulk create these new rmcs
+        RecordMaxCounter.objects.bulk_create([RecordMaxCounter(store_model_id=r_id, instance_id=current_id.id, counter=current_id.counter) for r_id in new_rmc_ids])
+        # clear deleted models table for this profile
         DeletedModels.objects.filter(profile=profile).delete()
+        # update our own database max counters after serialization
         if not filter:
             DatabaseMaxCounter.objects.update_or_create(instance_id=current_id.id, partition="", defaults={'counter': current_id.counter})
         else:
