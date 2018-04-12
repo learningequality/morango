29a3a2b2a84de322f165a370c5d63adf6d1fd90c
Rafael Aguayo <ralphaguayo22@gmail.com>
2018-04-12 15:56:31 -0700

    Update tests for new db router

---
+++ tests/testapp/testapp/postgres_test.py
@@ -0,0 +1,18 @@
+"""
+A settings module for running tests using a postgres db backend.
+"""
+from __future__ import absolute_import, print_function, unicode_literals
+
+from .settings import *  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'NAME': 'default',  # This module should never be used outside of tests -- so this name is irrelevant
+        'TEST': {
+            'NAME': 'travis_ci_default'
+        }
+    },
+}
