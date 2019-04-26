86413c7ab5b156ea7b0eac32d2fad89d7b7e39e6
José L. Redrejo Rodríguez <jredrejo@debian.org>
2019-04-26 20:58:47 +0200

    Add the needed path to the cache file

--- morango/constants/file.py
+++ morango/constants/file.py
@@ -1,1 +1,6 @@
-SQLITE_VARIABLE_FILE_CACHE = 'SQLITE_MAX_VARIABLE_NUMBER.cache'
+import pwd
+import os
+
+SQLITE_VARIABLE_FILE_CACHE = os.path.join(pwd.getpwuid(os.getuid()).pw_dir,
+                                          'SQLITE_MAX_VARIABLE_NUMBER.cache')
+
