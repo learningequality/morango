d4cd8e96bb8d0675511f0962305c1a59293b48e6
José L. Redrejo Rodríguez <jredrejo@debian.org>
2019-05-19 16:22:34 +0200

    pwd is not available in Windows

--- morango/constants/file.py
+++ morango/constants/file.py
@@ -1,6 +1,4 @@
-import pwd
 import os
 
-SQLITE_VARIABLE_FILE_CACHE = os.path.join(pwd.getpwuid(os.getuid()).pw_dir,
+SQLITE_VARIABLE_FILE_CACHE = os.path.join(os.path.expanduser('~'),
                                           'SQLITE_MAX_VARIABLE_NUMBER.cache')
-
