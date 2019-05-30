41bc7704b82e35257f19e23ac1ee9db08c21ee23
Rafael Aguayo <ralphaguayo22@gmail.com>
2019-05-29 19:05:18 -0700

    bump to 0.4.4

--- morango/__init__.py
+++ morango/__init__.py
@@ -1,4 +1,6 @@
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
 
 default_app_config = 'morango.apps.MorangoConfig'
-__version__ = "0.4.3"
+__version__ = "0.4.4"
