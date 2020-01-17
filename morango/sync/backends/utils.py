29fe2a151b7424394ecfab1d55d1649553b0c256
Rafael Aguayo <ralphaguayo22@gmail.com>
2020-01-17 15:33:06 -0800

    revert max var calculation because cte not supported in older sqlite version

--- morango/sync/backends/utils.py
+++ morango/sync/backends/utils.py
@@ -21,24 +21,28 @@ def calculate_max_sqlite_variables():
     SQLite has a limit on the max number of variables allowed for parameter substitution. This limit is usually 999, but
     can be compiled to a different number. This function calculates what the max is for the sqlite version running on the device.
     We use the calculated value to chunk our SQL bulk insert statements when deserializing from the store to the app layer.
+    Source: https://stackoverflow.com/questions/17872665/determine-maximum-number-of-columns-from-sqlite3
     """
     conn = sqlite3.connect(":memory:")
-    MAX_VARIABLE_NUMBER = 999
-    statement = """
-        WITH opts(n, opt) AS (
-        VALUES(0, NULL)
-        UNION ALL
-        SELECT n + 1,
-                sqlite_compileoption_get(n)
-        FROM opts
-        WHERE sqlite_compileoption_get(n) IS NOT NULL
-        )
-        SELECT CASE WHEN sqlite_compileoption_used('MAX_VARIABLE_NUMBER')
-            THEN (SELECT opt FROM opts WHERE opt LIKE 'MAX_VARIABLE_NUMBER%')
-            ELSE 'MAX_VARIABLE_NUMBER=999'
-        END;
-    """
-    cursor = conn.execute(statement)
-    output = cursor.fetchone()[0]
-    exec(output)
-    return MAX_VARIABLE_NUMBER
+    low = 1
+    high = (
+        1000
+    )  # hard limit for SQLITE_MAX_VARIABLE_NUMBER <http://www.sqlite.org/limits.html>
+    conn.execute("CREATE TABLE T1 (id C1)")
+    while low < high - 1:
+        guess = (low + high) // 2
+        try:
+            statement = "select * from T1 where id in (%s)" % ",".join(
+                ["?" for _ in range(guess)]
+            )
+            values = [i for i in range(guess)]
+            conn.execute(statement, values)
+        except sqlite3.DatabaseError as ex:
+            if "too many SQL variables" in str(ex):
+                high = guess
+            else:
+                raise
+        else:
+            low = guess
+    conn.close()
+    return low
