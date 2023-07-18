d74092a19be8adc70ae164cd144c45986e7a0b6e
Blaine Jester <blainesworld@gmail.com>
2023-07-18 13:44:48 -0700

    Try explicit imports

--- tests/testapp/tests/compat.py
+++ tests/testapp/tests/compat.py
@@ -1,11 +1,10 @@
-import sys
-
 try:
     # In the Python 2.7 GH workflows, we have to install backported version
-    if sys.version_info.major == 2:
-        from backports.test.support import EnvironmentVarGuard # noqa F401
-    else:
-        from test.support import EnvironmentVarGuard # noqa F401
+    from backports.test.support import EnvironmentVarGuard # noqa F401
 except ImportError:
-    # In Python 3.10, this has been moved to test.support.os_helper
-    from test.support.os_helper import EnvironmentVarGuard # noqa F401
+    try:
+        # For python >2.7 and <3.10
+        from test.support import EnvironmentVarGuard # noqa F401
+    except ImportError:
+        # In Python 3.10, this has been moved to test.support.os_helper
+        from test.support.os_helper import EnvironmentVarGuard # noqa F401
