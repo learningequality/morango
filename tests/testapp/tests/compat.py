854e333884e8f662dd4eacc2c8f1a441b5204045
Blaine Jester <blainesworld@gmail.com>
2023-07-18 13:39:13 -0700

    Import from backported for 2.7

--- tests/testapp/tests/compat.py
+++ tests/testapp/tests/compat.py
@@ -1,5 +1,11 @@
+import sys
+
 try:
-    from test.support import EnvironmentVarGuard # noqa F401
+    # In the Python 2.7 GH workflows, we have to install backported version
+    if sys.version_info.major == 2:
+        from backports.test.support import EnvironmentVarGuard # noqa F401
+    else:
+        from test.support import EnvironmentVarGuard # noqa F401
 except ImportError:
     # In Python 3.10, this has been moved to test.support.os_helper
     from test.support.os_helper import EnvironmentVarGuard # noqa F401
