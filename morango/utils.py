204671203b4543e008f481f841cc5137ed59984a
Blaine Jester <blainesworld@gmail.com>
2026-02-12 15:48:25 -0800

    Move model helper to shared location

--- morango/utils.py
+++ morango/utils.py
@@ -129,3 +129,14 @@ def _assert(condition, message, error_type=AssertionError):
     """
     if not condition:
         raise error_type(message)
+
+
+def self_referential_fk(klass_model):
+    """
+    Return whether this model has a self ref FK, and the name for the field
+    """
+    for f in klass_model._meta.concrete_fields:
+        if f.related_model:
+            if issubclass(klass_model, f.related_model):
+                return f.attname
+    return None
