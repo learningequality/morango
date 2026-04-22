bb5a11b4708ff376b09d3ac9b09fbeb2efeff23b
ozer550 <prathameshd419@gmail.com>
2026-04-22 16:48:49 +0530

    add _self_ref_order field

--- morango/models/core.py
+++ morango/models/core.py
@@ -6,6 +6,7 @@ from collections import defaultdict, namedtuple
 from functools import reduce
 
 from django.core import exceptions
+from django.core.validators import MinValueValidator
 from django.db import connection, models, router, transaction
 from django.db.models import F, Func, Max, Q, TextField, Value, signals
 from django.db.models.deletion import Collector
@@ -391,6 +392,9 @@ class AbstractStore(models.Model):
     conflicting_serialized_data = models.TextField(blank=True)
 
     _self_ref_fk = models.CharField(max_length=32, blank=True)
+    _self_ref_order = models.IntegerField(
+        blank=True, null=True, validators=[MinValueValidator(0)]
+    )
 
     class Meta:
         abstract = True
